a87de6c0a86793871bd74de54e66008fb41a5ca5
Renan Moura <renaan89@gmail.com>
2026-01-26 16:10:57 -0300

    add cors swagger

--- Services/CorsService.cs
+++ Services/CorsService.cs
@@ -6,7 +6,7 @@
         {
             services.AddCors(a => a
                 .AddPolicy("AllowFrontend", cors => cors
-                .WithOrigins("https://-hmg-9712aeff20c4.herokuapp.com",
+                .WithOrigins("https://fitapi-ecb3ac695201.herokuapp.com",
                              "https://localhost:7265",
                              "http://localhost:3030",
                              "http://localhost:8081")
